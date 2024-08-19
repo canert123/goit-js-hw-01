fddb5878dc1bbc5bdb6b0a24aef1f77818f33a74
canert123 <98495388+canert123@users.noreply.github.com>
2024-08-19 22:00:56 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -4,3 +4,7 @@ function getElementWidth(content, padding, border) {
   const border1 = parseFloat(border);
   console.log(`${content1 + padding1 * 2 + border1 * 2}`);
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
